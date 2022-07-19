bb235520ac906c713b09d502fbbc2d6490fd8740
Patrick Figel <pfigel@greenpeace.org>
2022-07-19 14:25:25 +0200

    GP-22919 Upgrade civix

--- hubspot.php
+++ hubspot.php
@@ -17,15 +17,6 @@ function hubspot_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function hubspot_civicrm_xmlMenu(&$files) {
-  _hubspot_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
@@ -80,54 +71,6 @@ function hubspot_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 }
 
 /**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function hubspot_civicrm_managed(&$entities) {
-  _hubspot_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function hubspot_civicrm_caseTypes(&$caseTypes) {
-  _hubspot_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function hubspot_civicrm_angularModules(&$angularModules) {
-  _hubspot_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function hubspot_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _hubspot_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
  * Implements hook_civicrm_entityTypes().
  *
  * Declare entity types provided by this module.
@@ -136,13 +79,6 @@ function hubspot_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
  */
 function hubspot_civicrm_entityTypes(&$entityTypes) {
   _hubspot_civix_civicrm_entityTypes($entityTypes);
-}
-
-/**
- * Implements hook_civicrm_themes().
- */
-function hubspot_civicrm_themes(&$themes) {
-  _hubspot_civix_civicrm_themes($themes);
 }
 
 function hubspot_civicrm_postCommit($op, $objectName, $objectId, &$objectRef) {

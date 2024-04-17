0d8f518586cbef71491a56db0d9cfa65b4d17283
Patrick Figel <pfigel@greenpeace.org>
2024-04-17 14:15:55 +0200

    GP-45185 Upgrade civix

--- CRM/Hubspot/Upgrader.php
+++ CRM/Hubspot/Upgrader.php
@@ -4,7 +4,7 @@ use CRM_Hubspot_ExtensionUtil as E;
 /**
  * Collection of upgrade steps.
  */
-class CRM_Hubspot_Upgrader extends CRM_Hubspot_Upgrader_Base {
+class CRM_Hubspot_Upgrader extends CRM_Extension_Upgrader_Base {
 
   public function upgrade_1001() {
     $this->ctx->log->info('Applying update 1001');

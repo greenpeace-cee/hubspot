6d6730d5e07524c5300d88450032cd5067131f88
Manuel Flandorfer <mflandor@greenpeace.org>
2026-09-04 16:19:56 +0200

    Hubspot.syncEvents: Update events after failed sync

--- CRM/Hubspot/BatchEventCreator.php
+++ CRM/Hubspot/BatchEventCreator.php
@@ -6,27 +6,28 @@ use CRM_Hubspot_ApiClient as ApiClient;
 class CRM_Hubspot_BatchEventCreator extends CRM_Hubspot_BatchProcessor {
 
   public static function processBatch(CRM_Queue_TaskContext $_context, array $batch): bool {
-    $batch_response = ApiClient::batchCreateEvents($batch);
+    $sync_failed = FALSE;
+
+    try {
+      ApiClient::batchCreateEvents($batch);
+    } catch (Exception $exception) {
+      $sync_failed = TRUE;
+    }
 
     $event_update = Api4\HubspotEvent::save(FALSE);
-
-    if ($batch_response->getStatusCode() !== 204) {
-      // @TODO: Handle error, mark events with sync_failed = TRUE
-      return FALSE;
-    }
 
     foreach ($batch as $event) {
       $event_update->addRecord([
         'id'          => $event['properties']['civicrm_id'],
-        'hubspot_id'  => $event['uuid'],
+        'hubspot_id'  => $sync_failed ? NULL : $event['uuid'],
         'sync_date'   => date('Y-m-d H:i:s'),
-        'sync_failed' => FALSE,
+        'sync_failed' => $sync_failed,
       ]);
     }
 
     $event_update->setMatch(['id'])->execute();
 
-    return TRUE;
+    return !$sync_failed;
   }
 
 }
